7e0aed4df2a5f18298e4a1e4e5c22ec2b34484c8
Sabuj Nandan <sabujnandan41@gmail.com>
2024-11-12 13:38:16 +0530

    updating resume

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,15 @@ export default {
       '150': '150vh',
       '120': '120vh',
       '115' : '115vh',
+      '47' : '47%',
     },
     gap:{
       '150' : '150px'
 
+    },
+    screens: {
+      'my': '374px',
+      // => @media (min-width: 992px) { ... }
     },
   },
     fontFamily: {
